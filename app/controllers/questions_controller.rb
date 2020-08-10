1ac15d847be5ec015febbca7d8d86b7e7f05b28c
Nelson Schildge <nelsonschildge@gmail.com>
2020-08-10 13:51:58 +0200

    stupid coach, testing wip

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+
+    if @question == 'I am going to work'
+      @answer = @answers[0]
+    elsif @question.end_with?('?')
+      @answer = @answers[1]
+    else
+      @answer = @answers[2]
+    end
+  end
+end
